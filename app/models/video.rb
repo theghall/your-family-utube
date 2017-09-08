814307be3a7ab1c4ba23222d5cebe343d823d062
Gary Hall <guategary@gmail.com>
2017-09-08 17:14:11 +0000

    Add dependent destroy

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
   belongs_to :profile
-  has_many :video_tags
+  has_many :video_tags, dependent: :destroy
   has_many :tags, through: :video_tags
   default_scope -> { order(created_at: :desc) }
   mount_uploader :thumbnail, ThumbnailUploader
