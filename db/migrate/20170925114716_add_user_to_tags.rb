b57103834ae254e5751d5c9ca2de8dad9bb728e3
Gary Hall <guategary@gmail.com>
2017-09-25 08:16:24 -0400

    Add reference to tag migration

--- db/migrate/20170925114716_add_user_to_tags.rb
+++ db/migrate/20170925114716_add_user_to_tags.rb
@@ -1,5 +1,6 @@
 class AddUserToTags < ActiveRecord::Migration[5.0]
   def change
+    add_reference :tags, :users
     add_foreign_key :tags, :users, column: :user_id, primary_key: 'user_id'
     add_index :tags, [:user_id, :name], unique: true
   end
