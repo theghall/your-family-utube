f4720b77b442572037b8102ad7a397c3d94faa92
Gary Hall <guategary@gmail.com>
2017-09-25 08:31:36 -0400

    Fix pk: user_id to id

--- db/migrate/20170925114716_add_user_to_tags.rb
+++ db/migrate/20170925114716_add_user_to_tags.rb
@@ -1,7 +1,7 @@
 class AddUserToTags < ActiveRecord::Migration[5.0]
   def change
     add_reference :tags, :user
-    add_foreign_key :tags, :users, column: :user_id, primary_key: 'user_id'
+    add_foreign_key :tags, :users, column: :user_id, primary_key: 'id'
     add_index :tags, [:user_id, :name], unique: true
   end
 end
