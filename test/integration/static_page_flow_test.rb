a1f7e3d34a333bebc9a78f15376a3130ca73911d
Gary Hall <guategary@gmail.com>
2017-10-22 14:46:07 -0400

    Add Help page

--- test/integration/static_page_flow_test.rb
+++ test/integration/static_page_flow_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StaticPageFlowTest < ActionDispatch::IntegrationTest
+  test "should display help  page" do
+    get help_path
+    assert_response :success
+  end
+
   test "should display FAQ page" do
     get faq_path
     assert_response :success
