f091944e0074cc3259c148c9823e9370c2213c99
Gary Hall <guategary@gmail.com>
2017-10-31 06:25:15 -0400

    Refactor for better interaction with password vaults

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   include ApplicationHelper
 
+  before_action :handle_reset, only: [:update]
   before_action :parent_user, only: [:edit]
 
   def create
@@ -8,6 +9,10 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def edit
+    super
+  end
+
+  def update
     super
   end
 
@@ -19,5 +24,13 @@ private
 
     def account_update_params
      params.require(:user).permit(:name, :email, :pin, :pin_confirmation, :password, :password_confirmation, :current_password)
+    end
+
+    def handle_reset
+      if params[:reset]
+        respond_to do |format|
+          format.html { render 'devise/registrations/edit' }
+        end
+      end
     end
 end
