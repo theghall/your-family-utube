e88b6f1b1bab907cde6cd309c462766f401b584f
Gary Hall <guategary@gmail.com>
2018-12-05 23:34:32 -0500

    Security update

--- Gemfile
+++ Gemfile
@@ -1,19 +1,19 @@
 source 'https://rubygems.org'
 
 # Framwork
-gem 'rails', '5.0.3'
+gem 'rails', '5.2.2'
 # Presentation
-gem 'sass-rails', '5.0.6'
+gem 'sass-rails', '5.0.7'
 gem 'bootstrap-sass', '3.3.7'
 gem 'will_paginate', '3.1.5'
 gem 'bootstrap-will_paginate', '1.0.0'
 # Assest utilities
 gem 'uglifier', '3.2.0'
-gem 'coffee-rails', '4.1.1'
+gem 'coffee-rails', '4.2.2'
 # Libraries
 # Devise: user authtentication
-gem 'devise', '4.3.0'
-gem 'jquery-rails', '4.3.1'
+gem 'devise', '4.5.0'
+gem 'jquery-rails', '4.3.3'
 gem 'jquery-ui-rails', '6.0.1'
 gem 'jquery-serialize-object-rails', '2.5.0.1'
 # Yt: Youtube API
@@ -41,8 +41,8 @@ end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
-  gem 'web-console', '3.5.1'
-  gem 'listen', '3.0.8'
+  gem 'web-console', '3.7.0'
+  gem 'listen', '3.1.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring', '2.0.2'
   gem 'spring-watcher-listen', '2.0.1'
--- db/schema.rb
+++ db/schema.rb
@@ -10,117 +10,117 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023174946) do
+ActiveRecord::Schema.define(version: 2017_10_23_174946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "account_types", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "video_limit"
-    t.integer  "max_videos"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["name"], name: "index_account_types_on_name", unique: true, using: :btree
-  end
-
-  create_table "general_settings", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "setting_id"
-    t.string   "value"
+  create_table "account_types", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.boolean "video_limit"
+    t.integer "max_videos"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["setting_id"], name: "index_general_settings_on_setting_id", using: :btree
-    t.index ["user_id", "setting_id"], name: "index_general_settings_on_user_id_and_setting_id", unique: true, using: :btree
-    t.index ["user_id"], name: "index_general_settings_on_user_id", using: :btree
+    t.index ["name"], name: "index_account_types_on_name", unique: true
   end
 
-  create_table "profile_settings", force: :cascade do |t|
-    t.integer  "profile_id"
-    t.integer  "setting_id"
-    t.string   "value"
+  create_table "general_settings", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "setting_id"
+    t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["profile_id", "setting_id"], name: "index_profile_settings_on_profile_id_and_setting_id", unique: true, using: :btree
-    t.index ["profile_id"], name: "index_profile_settings_on_profile_id", using: :btree
-    t.index ["setting_id"], name: "index_profile_settings_on_setting_id", using: :btree
+    t.index ["setting_id"], name: "index_general_settings_on_setting_id"
+    t.index ["user_id", "setting_id"], name: "index_general_settings_on_user_id_and_setting_id", unique: true
+    t.index ["user_id"], name: "index_general_settings_on_user_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.text     "name",       null: false
-    t.integer  "user_id"
+  create_table "profile_settings", id: :serial, force: :cascade do |t|
+    t.integer "profile_id"
+    t.integer "setting_id"
+    t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "name"], name: "index_profiles_on_user_id_and_name", unique: true, using: :btree
-    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+    t.index ["profile_id", "setting_id"], name: "index_profile_settings_on_profile_id_and_setting_id", unique: true
+    t.index ["profile_id"], name: "index_profile_settings_on_profile_id"
+    t.index ["setting_id"], name: "index_profile_settings_on_setting_id"
   end
 
-  create_table "settings", force: :cascade do |t|
-    t.string   "name"
-    t.string   "setting_type"
-    t.string   "default_value"
-    t.boolean  "strict_values"
-    t.string   "allowed_values"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.index ["name"], name: "index_settings_on_name", unique: true, using: :btree
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "name",       null: false
+  create_table "profiles", id: :serial, force: :cascade do |t|
+    t.text "name", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "name"], name: "index_tags_on_user_id_and_name", unique: true, using: :btree
-    t.index ["user_id"], name: "index_tags_on_user_id", using: :btree
+    t.index ["user_id", "name"], name: "index_profiles_on_user_id_and_name", unique: true
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name",                   default: "", null: false
-    t.string   "parent_digest",          default: "", null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "settings", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "setting_type"
+    t.string "default_value"
+    t.boolean "strict_values"
+    t.string "allowed_values"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_settings_on_name", unique: true
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.text "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "name"], name: "index_tags_on_user_id_and_name", unique: true
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "parent_digest", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "account_type_id"
-    t.index ["account_type_id"], name: "index_users_on_account_type_id", using: :btree
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "video_tags", force: :cascade do |t|
-    t.integer  "video_id"
-    t.integer  "tag_id"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["tag_id"], name: "index_video_tags_on_tag_id", using: :btree
-    t.index ["video_id"], name: "index_video_tags_on_video_id", using: :btree
+    t.integer "account_type_id"
+    t.index ["account_type_id"], name: "index_users_on_account_type_id"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "videos", force: :cascade do |t|
-    t.string   "youtube_id",                 null: false
-    t.boolean  "approved",   default: false, null: false
-    t.integer  "profile_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "thumbnail"
-    t.string   "title"
-    t.boolean  "has_cc"
-    t.index ["profile_id", "youtube_id"], name: "index_videos_on_profile_id_and_youtube_id", unique: true, using: :btree
-    t.index ["profile_id"], name: "index_videos_on_profile_id", using: :btree
+  create_table "video_tags", id: :serial, force: :cascade do |t|
+    t.integer "video_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_video_tags_on_tag_id"
+    t.index ["video_id"], name: "index_video_tags_on_video_id"
+  end
+
+  create_table "videos", id: :serial, force: :cascade do |t|
+    t.string "youtube_id", null: false
+    t.boolean "approved", default: false, null: false
+    t.integer "profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "thumbnail"
+    t.string "title"
+    t.boolean "has_cc"
+    t.index ["profile_id", "youtube_id"], name: "index_videos_on_profile_id_and_youtube_id", unique: true
+    t.index ["profile_id"], name: "index_videos_on_profile_id"
   end
 
   add_foreign_key "general_settings", "settings"
